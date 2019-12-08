a46e67c0b9d696a83ce683f79da3cc75d6ba1bdd
Spabreaks Team <sb-team@spabreaks.com>
2019-12-08 10:12:00 +0000

    Save model and make it available globally

--- app.py
+++ app.py
@@ -5,13 +5,17 @@ import inverse_cooking_model.inversecooking_main as ic
 
 app = Flask(__name__)
 
-ingr_size, instrs_size, _, _ = ic.load_vocabularies()
-ic.load_model(ingr_size, instrs_size)
+def initialize_model():
+    ingr_size, instrs_size, _, _ = ic.load_vocabularies()
+    return ic.load_model(ingr_size, instrs_size)
 
 @app.route('/predict', methods=['GET'])
 def predict():
-    r = Recipe(name="Hello", ingredients=["one","two"], steps=["first do this", "then do that"])
+    r = Recipe(name="Hello",
+               ingredients=["one","two"],
+               steps=["first do this", "then do that"])
     return jsonify(r)
 
+model = initialize_model()
 if __name__ == '__main__':
     app.run(debug=False, port=os.getenv('PORT', 8080))
